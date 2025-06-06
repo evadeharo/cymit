86d605db86fff36c4d30287a6a104789aeba1384
Eva De Haro <evadeharomunoz@gmail.com>
2025-06-06 21:43:48 +0200

    feat: add component for Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <nav className="fixed inset-0 h-16 z-50 flex items-center justify-center border-b border-px border-gray mx-12">
+      <a href="/" className="font-bold text-[1.6rem]">CYMIT</a>
+    </nav>
+  );
+}
